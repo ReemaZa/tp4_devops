8ec9783ddecd072839fc133418d34249dc5a2fd7
ReemaZa <rimazarrouki9@gmail.com>
2026-05-08 07:35:26 +0100

    test: add metrics endpoint test to pass quality gate

--- test_app.py
+++ test_app.py
@@ -2,10 +2,20 @@ import unittest
 from app import app
 
 class BasicTests(unittest.TestCase):
+    # Test de la route principale
     def test_index(self):
         tester = app.test_client(self)
         response = tester.get('/')
         self.assertEqual(response.status_code, 200)
+        self.assertIn(b"Hello INSAT!", response.data)
+
+    # NOUVEAU : Test de la route metrics pour SonarQube
+    def test_metrics(self):
+        tester = app.test_client(self)
+        response = tester.get('/metrics')
+        self.assertEqual(response.status_code, 200)
+        # On vérifie que les métriques Prometheus sont bien générées
+        self.assertIn(b"flask_app_requests_total", response.data)
 
 if __name__ == '__main__':
     unittest.main()
